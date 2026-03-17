82f78bc693610ad62889861373268f1edcde6753
Jordan Chow <jordanwilliamchow@gmail.com>
2026-03-17 12:30:19 -0700

    Added a checker to check a number was input and added $ % symbols to before user inputs.

--- peg-ratio-calc.py
+++ peg-ratio-calc.py
@@ -1,8 +1,19 @@
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
 def peg_ratio():
     print("\nPEG Ratio Calculator:")
-    price_per_share = float(input("\nType in price per share.\n"))
-    growth_rate = float(input("\nType in expected growth rate.\n"))
-    peg = f"{(price_per_share / growth_rate):.2f}"
+    price_per_share = input("\nType in price per share.\n$")
+    while is_float(price_per_share) == False:
+        price_per_share = input("\nA number was not typed in. Please tpye in price per share.\n$")
+    growth_rate = input("\nType in expected growth rate.\n%")
+    while is_float(growth_rate) == False:
+        growth_rate = input("\nA number was not typed in. Please tpye in a expected growth rate.\n%")
+    peg = f"{(float(price_per_share) / float(growth_rate)):.2f}"
     print("\nThe PEG ratio is: " + peg + ".\n")
 peg_ratio()
 
