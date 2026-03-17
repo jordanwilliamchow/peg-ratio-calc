36067364f94709bef952e9cdde30b1b7094e109e
Jordan Chow <jordanwilliamchow@gmail.com>
2026-03-17 00:49:33 -0700

    Fixed calculation of PEG ratio.

--- peg-ratio-calc.py
+++ peg-ratio-calc.py
@@ -1,10 +1,9 @@
 def peg_ratio():
-    print("\nPEG Ratio Calculator")
+    print("\nPEG Ratio Calculator:")
     price_per_share = float(input("\nType in price per share.\n"))
-    shares = float(input("\nType in outstanding shares.\n"))
     growth_rate = float(input("\nType in expected growth rate.\n"))
-    peg = f"{(price_per_share / shares / growth_rate):.2f}"
-    print("\nThe PEG ratio is: " + peg + ".")
+    peg = f"{(price_per_share / growth_rate):.2f}"
+    print("\nThe PEG ratio is: " + peg + ".\n")
 peg_ratio()
 
 
