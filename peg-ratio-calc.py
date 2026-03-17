30bd11d8b609fa585fde3a4aff70ac3d20eef12b
Jordan Chow <jordanwilliamchow@gmail.com>
2026-03-17 00:43:52 -0700

    Finished first pass of code.

--- peg-ratio-calc.py
+++ peg-ratio-calc.py
@@ -1,7 +1,11 @@
-price_per_share = 0
-shares = 0
-growth_rate = 0
+def peg_ratio():
+    print("\nPEG Ratio Calculator")
+    price_per_share = float(input("\nType in price per share.\n"))
+    shares = float(input("\nType in outstanding shares.\n"))
+    growth_rate = float(input("\nType in expected growth rate.\n"))
+    peg = f"{(price_per_share / shares / growth_rate):.2f}"
+    print("\nThe PEG ratio is: " + peg + ".")
+peg_ratio()
 
-def peg_ratio(price_per_share, shares, growth_rate):
-    return (price_per_share * shares) / growth_rate
+
 
